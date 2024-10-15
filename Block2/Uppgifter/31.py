d2ddc66f363618813f354568d9cb88481b7a389f
Andreas <131877497+Nievolve@users.noreply.github.com>
2024-10-15 16:36:32 +0200

    Update

---
+++ Block2/Uppgifter/31.py
@@ -0,0 +1,9 @@
+# Bilder nedan visar grafen till sinus- och cosinusfunktion
+# för vinklar i intervallet 0 <= x <= 2π rad. Använd graferna för att
+# få närmevärde till följande trigonometriska ekvationer i det
+# intervallet. Ange även lösningar i vinkelmått grader.
+
+# a) sin(x) = 0.6
+# b) sin(x) = -0.9
+# c) cos(x) = 0.6
+# d) cos(x) = -0.8
